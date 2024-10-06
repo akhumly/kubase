2233a41c872c1c2de517da82e17afb836c6e22b9
akhumly <164754171+akhumly@users.noreply.github.com>
2024-10-06 12:54:28 +0000

    Adding prisma and database schema

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 To start new dev environemnt
 cd backend
 npm install
+
+npm install dotenv
+
+npm install prisma @prisma/client
+
+
 npm run start:dev
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+}
+
+@Injectable()
+export class UserService {
+  private prisma = new PrismaClient();
+
+  async getUsers() {
+    return this.prisma.user.findMany();
   }
 }
