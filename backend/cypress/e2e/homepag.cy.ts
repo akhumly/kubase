4f8a31bf3231550383679d13d8165726bdbf345a
akhumly <164754171+akhumly@users.noreply.github.com>
2024-10-13 15:33:31 +0000

    Fresh start with testing

--- backend/cypress/e2e/homepag.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('API Test', () => {
-  it('should load the homepage', () => {
-    cy.request('/').then((response) => {
-      // Use full URL if baseUrl isn't working
-      expect(response.status).to.eq(200);
-    });
-  });
-});
